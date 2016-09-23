3b11b64e63cf4ab765b3a1caa5acabaa592d8408
lukeyy33 <b3030652@my.shu.ac.uk>
2016-09-23 18:21:09 +0100

    Continued

---
+++ projects.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+<script type ="text/javascript" src="scripts/script.js">
+</script>
+<link rel="stylesheet" type="text/css" href= "styles/main.css">
+
+    <title>My projects</title>
+<?php
+include 'navbar.php';
+
+?>
+</head>
+<body>
+    <div id="nav">
+	   <ul>
+		<li><a href="/index.html">Index</a></li>
+		<li><a href="/contact.html">Contact</a></li>
+        <li class="dropdown"><a href="/projects.html" class="dropMenu">Projects</a>
+            <div class="dropdown-content">
+              <a href="#">Paradiddles</a>
+              <a href="#">AAAF</a>
+              <a href="#">Link 3</a>
+        </div></li>
+	</ul>
+</div>
+
+    <div id="container">
+    <h1>My Projects</h1>
+
+
+    </div>
+</body>
+</html>
